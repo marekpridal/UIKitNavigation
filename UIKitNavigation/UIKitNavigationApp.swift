cba77921798bbf24f49fb20903ba5742595abf2b
Marek Pridal <marek@marekpridal.eu>
2025-04-13 11:14:11 +0200

    Initial Commit

---
+++ UIKitNavigation/UIKitNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIKitNavigationApp.swift
+//  UIKitNavigation
+//
+//  Created by Marek Pridal on 13.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct UIKitNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
